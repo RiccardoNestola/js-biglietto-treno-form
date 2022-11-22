1b4c6bcabce9d13903dc5e7170da3d31788f66c6
RiccardoNestola <riccardinux@gmail.com>
2022-11-22 17:19:34 +0100

    bugfix on discount

--- js/script.js
+++ js/script.js
@@ -41,16 +41,16 @@ button.addEventListener("click",function(){
     console.log(ageInput.value + "anni");
 
     if (parseInt(ageInput.value)  < 18) {
-        console.log(priceTicket.value);
-        output.innerHTML = "Il prezzo del biglietto è di " + (priceTicket = ((priceTicket * 17.5) / 100)).toFixed(2) + "€";
+        console.log(priceTicket);
+        output.innerHTML = "Il prezzo del biglietto è di " + (priceTicket - ((priceTicket * 17.5) / 100)).toFixed(2) + "&euro;";
     }
     else if (parseInt(ageInput.value) >= 65) {
         console.log(priceTicket);
-        output.innerHTML = "Il prezzo del biglietto è di " + (priceTicket = ((priceTicket * 33.3) / 100)).toFixed(2) + "€";
+        output.innerHTML = "Il prezzo del biglietto è di " + (priceTicket - ((priceTicket * 33.3) / 100)).toFixed(2) + "&euro;";
     }
     else  {
         console.log(priceTicket);
-        output.innerHTML = "Il prezzo del biglietto è di " + priceTicket.toFixed(2) + "€";
+        output.innerHTML = "Il prezzo del biglietto è di " + priceTicket.toFixed(2) + "&euro;";
     }
         
     })
